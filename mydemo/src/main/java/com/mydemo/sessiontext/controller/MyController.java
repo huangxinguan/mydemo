538bea06176ba685b60c82da7ac6786326a52099
huangxinguan <825937316@qq.com>
2020-10-26 17:38:49 +0800

    mydemo

--- mydemo/src/main/java/com/mydemo/sessiontext/controller/MyController.java
+++ mydemo/src/main/java/com/mydemo/sessiontext/controller/MyController.java
@@ -1,10 +1,24 @@
 package com.mydemo.sessiontext.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mydemo.common.Result;
 
 @RestController
 public class MyController {
 	
+	
+	@RequestMapping(value = "test1", method = RequestMethod.POST )
+	public Result test1(HttpServletRequest request, HttpServletResponse response) {
+		
+		
+		return Result.respSuccess();
+	}
 	
 
 }
