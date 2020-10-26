5026d42459ae73de674d3e47277518baac322caf
huangxinguan <huangxinguan@users.noreply.github.com>
2020-10-26 23:01:39 +0800

    mydemo

--- mydemo/src/main/java/com/mydemo/sessiontest/controller/MyController.java
+++ mydemo/src/main/java/com/mydemo/sessiontest/controller/MyController.java
@@ -2,23 +2,48 @@ package com.mydemo.sessiontest.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mydemo.common.Result;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import net.bytebuddy.asm.Advice.This;
+
 @RestController
+@Api(value = "test")
 public class MyController {
 	
-	
-	@RequestMapping(value = "test1", method = RequestMethod.POST )
+	private HttpSession httpSession;  
+	private HttpServletRequest request;
+	@ApiOperation(value = "test1")
+	@RequestMapping(value = "/test1", method = RequestMethod.POST )
 	public Result test1(HttpServletRequest request, HttpServletResponse response) {
+		System.out.println(httpSession == request.getSession());
+		httpSession = request.getSession();
 		
-		
+		System.out.println("request" + (this.request == request));
+		this.request = request;
 		return Result.respSuccess();
 	}
+	
+	@ApiOperation(value = "test2")
+	@RequestMapping(value = "/test2", method = RequestMethod.POST )
+	public Result test2(HttpServletRequest request, HttpServletResponse response) {
+		System.out.println(httpSession == request.getSession());
+		httpSession = request.getSession();
+		
+		System.out.println("request" + (this.request == request));
+		this.request = request;
+		return Result.respSuccess();
+	}
+	
+	
 	
 
 }
