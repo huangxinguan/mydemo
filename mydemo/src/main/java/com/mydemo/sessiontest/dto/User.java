7b9a32df41fd66d05a097c158022af8a11db4498
huangxinguan <825937316@qq.com>
2020-10-26 17:57:53 +0800

    mydemo

--- mydemo/src/main/java/com/mydemo/sessiontest/dto/User.java
+++ mydemo/src/main/java/com/mydemo/sessiontest/dto/User.java
@@ -1,5 +1,13 @@
 package com.mydemo.sessiontest.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class User {
 
+	private String username;
+	
+	private String password;
 }
