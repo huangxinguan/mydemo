e959097ac167d6404da8333cba0086f18cb8bada
huangxinguan <huangxinguan@users.noreply.github.com>
2020-10-26 23:45:00 +0800

    mydemo

--- mydemo/src/main/java/com/mydemo/sessiontest/controller/MyController.java
+++ mydemo/src/main/java/com/mydemo/sessiontest/controller/MyController.java
@@ -4,7 +4,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +12,6 @@ import com.mydemo.common.Result;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import net.bytebuddy.asm.Advice.This;
 
 @RestController
 @Api(value = "test")
@@ -22,23 +20,30 @@ public class MyController {
 	private HttpSession httpSession;  
 	private HttpServletRequest request;
 	@ApiOperation(value = "test1")
-	@RequestMapping(value = "/test1", method = RequestMethod.POST )
+	@RequestMapping(value = "/test1", method = RequestMethod.GET )
 	public Result test1(HttpServletRequest request, HttpServletResponse response) {
-		System.out.println(httpSession == request.getSession());
+//		System.out.println(httpSession == request.getSession());
 		httpSession = request.getSession();
-		
-		System.out.println("request" + (this.request == request));
+		System.out.println(request.getSession());
+		System.out.println(request.getRequestedSessionId());
+		System.out.println(request);
+		System.out.println(response);
+//		System.out.println("request" + (this.request == request));
 		this.request = request;
 		return Result.respSuccess();
 	}
 	
 	@ApiOperation(value = "test2")
-	@RequestMapping(value = "/test2", method = RequestMethod.POST )
+	@RequestMapping(value = "/test2", method = RequestMethod.GET )
 	public Result test2(HttpServletRequest request, HttpServletResponse response) {
 		System.out.println(httpSession == request.getSession());
 		httpSession = request.getSession();
-		
-		System.out.println("request" + (this.request == request));
+		System.out.println(request);
+		System.out.println(httpSession);
+		System.out.println(response);
+		System.out.println(request.getRequestedSessionId());
+
+//		System.out.println("request" + (this.request == request));
 		this.request = request;
 		return Result.respSuccess();
 	}
