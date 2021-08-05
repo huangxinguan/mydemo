f1d27d528597bbbaf8d31941b0987197af92a7fc
huangxinguan <825937316@qq.com>
2021-08-05 15:49:20 +0800

    leetCode

--- mydemo/src/main/java/com/mydemo/leetcode/LeetCode18.java
+++ mydemo/src/main/java/com/mydemo/leetcode/LeetCode18.java
@@ -1,5 +1,48 @@
 package com.mydemo.leetcode;
 
-public class LeetCode18 {
+import java.util.Arrays;
 
+public class LeetCode18 {
+	//[1,1,1],[1,1,0],[1,0,1]
+	public static void main(String[] args) {
+		int[][] image = new int[][] {{1, 1, 1}, {1, 1, 0}, {1, 0, 1}};
+		
+		int[][] floodFill = floodFill(image, 1, 1, 2);
+		System.out.println(Arrays.toString(floodFill[0]));
+		System.out.println(Arrays.toString(floodFill[1]));
+		System.out.println(Arrays.toString(floodFill[2]));
+		
+	}
+	
+	public static int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+		int[][] visit = new int[image.length][image[0].length];
+		dfs(image, sr, sc, newColor, image[sr][sc], visit);
+		return image;
+	}
+	
+	public static void dfs(int[][] image, int sr, int sc, int newColor, int a, int[][] visit) {
+		if(sr < 0 || sc < 0) {
+			return;
+		}
+		if(sr > image.length - 1 || sc > image[0].length - 1) {
+			return;
+		}
+		if(visit[sr][sc] != 0) {
+			return;
+		}
+		if(a != image[sr][sc]) {
+			return;
+		}
+		int temp = image[sr][sc];
+		visit[sr][sc] = 1;
+		image[sr][sc] = newColor;
+		//遍历左边的点
+		dfs(image, sr, sc - 1, newColor, temp, visit);
+		//遍历上边的点
+		dfs(image, sr - 1, sc, newColor, temp, visit);
+		//遍历右边的点
+		dfs(image, sr, sc + 1, newColor, temp, visit);
+		////遍历下边的点
+		dfs(image, sr + 1, sc, newColor, temp, visit);
+	}
 }
---
+++ mydemo/src/main/java/com/mydemo/leetcode/LeetCode19.java
@@ -0,0 +1,44 @@
+package com.mydemo.leetcode;
+
+public class LeetCode19 {
+	public static void main(String[] args) {
+		int[][] grid = {{0,0,1,0,0,0,0,1,0,0,0,0,0},
+				 {0,0,0,0,0,0,0,1,1,1,0,0,0},
+				 {0,1,1,0,1,0,0,0,0,0,0,0,0},
+				 {0,1,0,0,1,1,0,0,1,0,1,0,0},
+				 {0,1,0,0,1,1,0,0,1,1,1,0,0},
+				 {0,0,0,0,0,0,0,0,0,0,1,0,0},
+				 {0,0,0,0,0,0,0,1,1,1,0,0,0},
+				 {0,0,0,0,0,0,0,1,1,0,0,0,0}};
+		System.out.println(maxAreaOfIsland(grid));
+
+	}
+	public static int maxAreaOfIsland(int[][] grid) {
+		int max = 0;
+		int[][] visit = new int[grid.length][grid[0].length];
+		for(int i = 0; i < grid.length; i++) {
+			for(int j = 0; j < grid[0].length; j++) {
+				if(grid[i][j] == 0 || visit[i][j] == 1) {
+					continue;
+				}
+				int dfs = dfs(grid, i, j, visit);
+				max = dfs > max ? dfs : max; 		
+			}
+		}
+		return max;
+    }
+	public static int dfs(int[][] grid, int row, int col, int[][] visit){
+		if(row < 0 || col < 0 || row > grid.length - 1 || col > grid[0].length - 1 || visit[row][col] == 1) {
+			return  0;
+		}
+		if(grid[row][col] == 0) {
+			return 0;
+		}
+		visit[row][col] = 1;
+		int left = dfs(grid, row, col - 1, visit);
+		int up = dfs(grid, row - 1, col, visit);
+		int right = dfs(grid, row, col + 1, visit);
+		int down = dfs(grid, row + 1, col, visit);
+		return 1 + left + up + right + down;
+	}
+}
